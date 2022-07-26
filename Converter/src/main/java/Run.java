6517bedeff0f9570701fac447ab3ac101beff17f
VadimPrishedko <vadimprishedko@gmail.com>
2022-07-26 13:44:14 +0300

    Initial commit

--- Converter/src/main/java/Run.java
+++ Converter/src/main/java/Run.java
@@ -2,7 +2,6 @@ import service.FilesDir;
 
 public class Run {
     public static void main(String[] args) {
-//        /Users/user/Desktop/fileConverter/Converter
         FilesDir fd = new FilesDir();
         try {
             String dir = args[0];
